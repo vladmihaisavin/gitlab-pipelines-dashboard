a123b7d9e0eb00bcfbf7d8d1689b89bcacf4acb8
Vlad Mihai Savin <savin.vladmihai@gmail.com>
2021-07-18 20:15:25 +0300

    improved dockerfile

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 docker build --tag gitlab-pipelines-dashboard .
-docker run -p 3000:3000 -p 6606:6606 --name=erb -d  gitlab-pipelines-dashboard
+docker run -p 3000:3000 -p 6606:6606 --name=gpd -d  gitlab-pipelines-dashboard
